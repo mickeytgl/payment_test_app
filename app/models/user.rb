fe220298bd88a881ca3754155caa8cf63a116823
Miguel Torres <migueltg93@gmail.com>
2017-10-19 10:25:54 +0200

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :masqueradable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  validates :name, presence: true
+
+  def first_name
+    name.split(' ').first
+  end
+end
