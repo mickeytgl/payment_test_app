dc08e89958b8a3d0260225d68e7c1295c0e49b94
Miguel Torres <migueltg93@gmail.com>
2017-10-20 21:48:37 +0200

    Added Stripe gem, keys

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -1,48 +1,62 @@
 document.addEventListener("turbolinks:load", function() {
-  var public_key = document.querySelector("meta[name='stripe-public-key']").content;
-  var stripe = Stripe(public_key);
-  var elements = stripe.elements();
+// Create a Stripe client
+var stripe = Stripe('pk_test_AP2bhfyUE3LCkUKNgBqz9stU');
 
-  // Custom styling can be passed to options when creating an Element.
-  var style = {
-    base: {
-      // Add your base input styles here. For example:
-      fontSize: '16px',
-      lineHeight: '24px'
+// Create an instance of Elements
+var elements = stripe.elements();
+
+// Custom styling can be passed to options when creating an Element.
+// (Note that this demo uses a wider set of styles than the guide below.)
+var style = {
+  base: {
+    color: '#32325d',
+    lineHeight: '24px',
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: 'antialiased',
+    fontSize: '16px',
+    '::placeholder': {
+      color: '#aab7c4'
     }
-  };
+  },
+  invalid: {
+    color: '#fa755a',
+    iconColor: '#fa755a'
+  }
+};
 
-  // Create an instance of the card Element
-  var card = elements.create('card', {style: style});
+// Create an instance of the card Element
+var card = elements.create('card', {style: style});
 
-  // Add an instance of the card Element into the `card-element` <div>
-  card.mount('#card-element');
+// Add an instance of the card Element into the `card-element` <div>
+card.mount('#card-element');
 
-  card.addEventListener('change', function(event) {
-    var displayError = document.getElementById('card-errors');
-    if (event.error) {
-      displayError.textContent = event.error.message;
+// Handle real-time validation errors from the card Element.
+card.addEventListener('change', function(event) {
+  var displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+// Handle form submission
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the user if there was an error
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
     } else {
-      displayError.textContent = '';
+      // Send the token to your server
+      console.log(result);
+      //stripeTokenHandler(result.token);
     }
   });
-
-  // Create a token or display an error when the form is submitted.
-  var form = document.getElementById('payment-form');
-  form.addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    stripe.createToken(card).then(function(result) {
-      if (result.error) {
-        // Inform the user if there was an error
-        var errorElement = document.getElementById('card-errors');
-        errorElement.textContent = result.error.message;
-      } else {
-        // Send the token to your server
-        stripeTokenHandler(result.token);
-      }
-    });
-  });
+});
 });
 
 function stripeTokenHandler(token) {
--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,4 +1,7 @@
 class SubscriptionsController < ApplicationController 
+	before_action :authenticate_user!, except: [:new]
+	before_action :redirect_to_signup, only: [:new]
+
 	def show
 	end
 
@@ -9,5 +12,32 @@ class SubscriptionsController < ApplicationController
 	end
 
 	def create
+		customer =	if current_user.stripe_id?
+					Stripe::Customer.retrieve(current_user.stripe_id)
+					else
+					Stripe::Customer.create(email: current_user.email)
+					end
+		
+		subscription = customer.subscriptions.create( 
+			source: params[:stripeToken],
+			plan: "monthly"
+		)
+
+		customer_user.update(
+			stripe_id: customer.id,
+			stripe_subscription_id: subscription.id
+		)
+		
+		redirect_to root_path	
 	end
+
+
+	private 
+
+		def redirect_to_signup
+			if !user_signed_in?
+				session["user_return_to"] = new_subscription_path
+				redirect_to new_user_registration_path 
+			end
+		end
 end
