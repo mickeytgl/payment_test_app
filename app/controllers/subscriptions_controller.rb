1e364ce37906e5417e58115dc28af4949e217a98
Miguel Torres <migueltg93@gmail.com>
2017-10-27 15:48:07 +0200

    Added payment gem

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,37 +1,37 @@
 class SubscriptionsController < ApplicationController 
-	before_action :authenticate_user!, except: [:new]
-	before_action :redirect_to_signup, only: [:new]
+  before_action :authenticate_user!, except: [:new]
+  before_action :redirect_to_signup, only: [:new]
 
-	def show
-	end
+  def show
+  end
 
-	def new
-	end
+  def new
+  end
 
 
-	def create
-		customer =	if current_user.stripe_id?
-					Stripe::Customer.retrieve(current_user.stripe_id)
-					else
-					Stripe::Customer.create(email: current_user.email)
-					end
+  def create
+    customer =	if current_user.stripe_id?
+				  Stripe::Customer.retrieve(current_user.stripe_id)
+				else
+			      Stripe::Customer.create(email: current_user.email)
+				end
 		
-		subscription = customer.subscriptions.create( 
-			source: params[:stripeToken],
-			plan: "monthly"
-		)
+	subscription = customer.subscriptions.create( 
+	  source: params[:stripeToken],
+	  plan: "monthly"
+	)
 
-		current_user.update(
-			stripe_id: customer.id,
-			stripe_subscription_id: subscription.id,
-			card_last4: params[:card_last4],
-			card_type: params[:card_brand],
-			card_exp_month: params[:card_exp_month],
-			card_exp_year: params[:card_exp_year]
-		)
+	current_user.update(
+	  stripe_id: customer.id,
+	  stripe_subscription_id: subscription.id,
+	  card_last4: params[:card_last4],
+	  card_type: params[:card_brand],
+	  card_exp_month: params[:card_exp_month],
+	  card_exp_year: params[:card_exp_year]
+	)
 		
-		redirect_to root_path	
-	end
+	redirect_to root_path	
+  end
 
 	def destroy
 		customer = Stripe::Customer.retrieve(current_user.stripe_id)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020153539) do
+ActiveRecord::Schema.define(version: 20171019125734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,12 +61,6 @@ ActiveRecord::Schema.define(version: 20171020153539) do
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "stripe_id"
-    t.string "stripe_subscription_id"
-    t.string "card_last4"
-    t.integer "card_exp_month"
-    t.integer "card_exp_year"
-    t.string "card_type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
